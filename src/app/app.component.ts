eb0317b31d659dd59142069ba1503874256e8859
David Vargas <dav793@gmail.com>
2018-12-17 12:56:41 -0600

    integrate navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,12 @@ import { Component } from '@angular/core';
 
 import { DataService } from './data.service';
 
-import { User } from './user/user.model';
-
-import * as moment from 'moment';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  isLoggedIn = false;
 
   constructor(
     private dataService: DataService
@@ -29,23 +23,10 @@ export class AppComponent {
 
   goToLogIn() {
     console.log('navigate to log in');
-    this.isLoggedIn = true;
   }
 
   logOut() {
     console.log('logging out...');
-    this.isLoggedIn = false;
-  }
-
-  get loggedUser(): User|null {
-    if (this.isLoggedIn)
-      return new User({
-        id: '1',
-        username: 'pepito',
-        fullName: 'Pepito Apellido'
-      });
-    else
-      return null;
   }
 
 }
