6944fd402635ff6fd33b276dda3fb5cd92033cfc
FelipeSJ <felipesolisjimenez@hotmail.com>
2018-12-21 17:26:18 -0600

    Se agregó user-login.component, el user.service, se importaron cosas en el app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { DataService } from './data.service';
 import { PostModule } from './post/post.module';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
-    PostModule
+    PostModule,
+    ReactiveFormsModule
   ],
   providers: [
     DataService
