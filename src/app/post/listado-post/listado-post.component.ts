d52c97c8868d813cdb60922d9aa9ea4dc92167d3
andresnboza <andresnboza92@gmail.com>
2019-01-13 19:41:55 -0600

    Listo el form de filtrado por medio de tags

--- src/app/post/listado-post/listado-post.component.ts
+++ src/app/post/listado-post/listado-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { DataService } from '../../data.service';
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-listado-post',
@@ -9,11 +10,32 @@ import { DataService } from '../../data.service';
 })
 export class ListadoPostComponent implements OnInit {
   allPost: Post[];
-  allTags: string[];
+  allTags: string[] = [];
 
-  constructor(private dataService: DataService) { }
+  searchForm = this.formBuilder.group({
+    search: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder, private dataService: DataService) { }
 
   ngOnInit() {
     this.allPost = this.dataService.findAll('posts');
+    this.fillAllTags();
+    // Crear servicio de filtrado por medio de los parametros, y hacerle un interface al filtrado al parametro del filtrado
+    // this.allTags = this.filterServiceByTags({tags: 'perros'});
+  }
+
+  fillAllTags() {
+    for (let post of this.allPost) {
+      for (let tag of post.tags) {
+        if (this.allTags.indexOf(tag) === -1) {
+          this.allTags.push(tag);
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    console.log(this.searchForm.value);
   }
 }
