63da2324bad96489544dbb0b9b5ce21c409bc22c
leoba11 <leobarri.2013@gmail.com>
2019-03-01 17:53:22 -0600

    cambios en el edit

--- src/app/post/post-edit/post-edit.component.ts
+++ src/app/post/post-edit/post-edit.component.ts
@@ -7,6 +7,9 @@ import { Post, PostFactory } from '../post.model';
 import * as moment from 'moment';
 import { DataService } from 'src/app/data.service';
 
+import {MatChipInputEvent} from '@angular/material';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+
 
 @Component({
   selector: 'app-post-edit',
@@ -17,107 +20,148 @@ export class PostEditComponent implements OnInit {
 
   postId: string;
   post: Post;
- 
-  postForm: FormGroup;
-  tagsForm: FormGroup;
 
-  pageTitleCreate = 'Create Post';
-  pageTitleEdit = 'Edit Post';
-  model: Post = PostFactory.CreateDefault();
+  postForm: FormGroup;
+  // tagsForm: FormGroup;
+
+  model = PostFactory.CreateDefault();
   preview: boolean;
-  
+
   actualDate = moment().format('D/MM/YYYY');
 
-  tags = ['Marvel', 'DC', 'Malabares', 'Celulares', 'Breaking Bad', 'Samsung', 'Iphone'];
+  // for the tags
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagsList = [];
+  tagsTest = ['Marvel', 'DC', 'Malabares', 'Celulares', 'Breaking Bad', 'Samsung', 'Iphone'];
 
-  
 
-  constructor( private formBuilder: FormBuilder, 
-               private router: Router, 
-               private service: DataService,
-               private route: ActivatedRoute ) { }
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    private dataService: DataService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    if (!this.isCreate) {
+      this.postId = this.route.snapshot.paramMap.get('id');
+      let singlePost = this.dataService.findById('posts', this.postId);
+      this.model = new Post(singlePost);
+      this.tagsList = this.model.tags;
+    }
+
     this.postForm = this.createFormWithBuilder(this.model);
-    this.tagsForm = this.createTagsForm(this.tags);
+    // this.tagsForm = this.createTagsForm(this.tagsList);
   }
 
   createFormWithBuilder(model: Post): FormGroup {
     const group = this.formBuilder.group({
-      title: [this.model.title, [Validators.required, Validators.minLength(3)]], 
-      date: {value: this.actualDate, disabled: true},
+      title: [this.model.title, [Validators.required, Validators.minLength(3)]],
+      date: { value: this.actualDate, disabled: true },
       tags: [this.model.tags, Validators.required],
       body: [this.model.body, [Validators.required]]
     });
     return group;
   }
 
-  createTagsForm(tags: string[]): FormGroup {
-    let tagsObj = {};
-    tags.forEach(tag => {
-      tagsObj[tag] = [false, []];
-    });
+  // createTagsForm(tags: string[]): FormGroup {
+  //   let tagsObj = {};
+  //   tags.forEach(tag => {
+  //     tagsObj[tag] = [false, []];
+  //   });
 
-    const group = this.formBuilder.group(tagsObj);
-    this.watchTagsForm(group);
+  //   const group = this.formBuilder.group(tagsObj);
+  //   this.watchTagsForm(group);
 
-    return group;
-  }
+  //   return group;
+  // }
 
-  watchTagsForm(group: FormGroup) {
-    let tags = Object.keys(group.controls);
+  // watchTagsForm(group: FormGroup) {
+  //   let tags = Object.keys(group.controls);
 
-    tags.forEach(tag => {
-      group.get(tag).valueChanges.subscribe(change => {
+  //   tags.forEach(tag => {
+  //     group.get(tag).valueChanges.subscribe(change => {
 
-        if (change === true) {    // agregar a tags
+  //       if (change === true) {    // agregar a tags
 
-          let selectedTags = this.selectedTags;
-          if (selectedTags.indexOf(tag) === -1) {   // no existe interest en elemento del form
-            selectedTags.push(tag);
-            this.postForm.get('tags').setValue(selectedTags);
-          }
+  //         let selectedTags = this.selectedTags;
+  //         if (selectedTags.indexOf(tag) === -1) {   // no existe interest en elemento del form
+  //           selectedTags.push(tag);
+  //           this.postForm.get('tags').setValue(selectedTags);
+  //         }
 
-        }
-        else {                    // remover de tags
+  //       }
+  //       else {                    // remover de tags
 
-          let selectedTags = this.selectedTags;
-          let tagIndex = selectedTags.indexOf(tag);
-          if (tagIndex !== -1) {   // existe tag en elemento del form
-            selectedTags.splice(tagIndex, 1);
-            this.postForm.get('tags').setValue(selectedTags);
-          }
+  //         let selectedTags = this.selectedTags;
+  //         let tagIndex = selectedTags.indexOf(tag);
+  //         if (tagIndex !== -1) {   // existe tag en elemento del form
+  //           selectedTags.splice(tagIndex, 1);
+  //           this.postForm.get('tags').setValue(selectedTags);
+  //         }
 
-        }
+  //       }
 
-      });
-    });
-  }
+  //     });
+  //   });
+  // }
 
-  get selectedTags(): string[] {
-    if (!this.postForm)
-      return null;
-    return this.postForm.get('tags').value;
-  }
-
-  setPreview() {
-    this.preview = !this.preview;
-  }
+  // get selectedTags(): string[] {
+  //   if (!this.postForm)
+  //     return null;
+  //   return this.postForm.get('tags').value;
+  // }
 
   buttonAction() {
 
-    // console.log(this.router.url.split('/'));
-
-    let ruta = this.router.url.split('/');
-    
-    if(ruta[2] === "create") {
-      let newPost = this.service.create('posts', this.postForm.value);
+    if (this.isCreate) {
+      let newPost = this.dataService.create('posts', this.postForm.value);
       console.log(newPost);
-    }else {
-      let id = this.route.snapshot.paramMap.get('id');
-
-      let updatedPost = this.service.updateById('posts', id , this.postForm.value);
+    } else {
+      let updatedPost = this.dataService.updateById('posts', this.postId, this.postForm.value);
       console.log(updatedPost);
+    }
+  }
+
+  get isCreate() {
+    let ruta = this.router.url.split('/');
+
+    if (ruta[2] === "create")
+      return true;
+    else
+      return false;
+  }
+
+  
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our tag
+    if ((value || '').trim()) {
+      this.tagsList.push(value.trim());
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  addInArray(tag: string) {
+    // Add our tag
+    if ((tag || '').trim()) {
+      this.tagsList.push(tag.trim());
+    }
+  }
+
+  remove(tag: string): void {
+    const index = this.tagsList.indexOf(tag);
+
+    if (index >= 0) {
+      this.tagsList.splice(index, 1);
     }
   }
 }
